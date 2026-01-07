e45da4ec828deefe7d712870d26562568310a473
ABHINDHIRAKP <abhindhira.kp2022ai-ml@sece.ac.in>
2026-01-07 11:42:21 +0530

    job model has been created.

---
+++ hiresense-backend/models/jobModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    recruiter: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    minExperience: {
+      type: Number,
+      required: true
+    },
+    skills: {
+      type: [String],
+      required: true
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Job', jobSchema);
