181ae8d3d154a0e84d519ccbcca7a31abf86b7c3
ABHINDHIRAKP <abhindhira.kp2022ai-ml@sece.ac.in>
2026-01-09 12:17:50 +0530

    view jobs feature added for recruiter and jbseeker

--- hiresense-backend/controllers/jobController.js
+++ hiresense-backend/controllers/jobController.js
@@ -22,6 +22,36 @@ const createJob = async (req, res) => {
     });
 }
 
+// @GET /api/jobs
+const getJobs = async (req, res) => {
+    if (!req.user || !req.user.roles) {
+        return res.status(401).json({message: "Unauthorized"});
+    }
+
+    // check if recruiter or jobseeker
+    const filter = {};
+    if (req.user.roles.includes('recruiter')) {
+        filter.recruiter = req.user.id;
+    }
+
+    /* fetch jobs:
+         - select only necessary fields
+         - sort by creation date descending
+         - if recruiter, then only the recruiter's jobs
+         - if jobseeker, then all jobs
+    */
+    const jobs = await Job.find(filter)
+    .select("title description recruiter")
+    .sort({ createdAt: -1 })
+    .populate("recruiter", "name email");
+
+    res.status(200).json({
+        message: "Jobs fetched successfully",
+        jobs
+    });
+}
+
 module.exports = {
-    createJob
+    createJob,
+    getJobs
 };
--- hiresense-backend/routes/jobRoutes.js
+++ hiresense-backend/routes/jobRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createJob } = require('../controllers/jobController');
+const { createJob, getJobs } = require('../controllers/jobController');
 const validateToken = require('../middleware/validateTokenHandler');
 const authRole = require('../middleware/authoriseRole');
 
-// execution flow -> POST '/' -> validateToken (sets req.user) -> authRole (checks roles) -> createJob
 router.use(validateToken);
+// execution flow -> POST '/' -> validateToken (sets req.user) -> authRole (checks roles) -> createJob
 router.route('/').post(authRole("recruiter"), createJob);
+
+// execution flow -> GET '/' -> validateToken (sets req.user) -> getJobs
+router.route('/').get(getJobs);
 
 module.exports = router;
